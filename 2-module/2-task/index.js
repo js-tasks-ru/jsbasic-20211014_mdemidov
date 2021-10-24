7e3d68d1b9ce4806526121faa1bb42264de0b007
Maxim Demidov <mdemidov@list.ru>
2021-10-24 12:43:32 +0300

    Задачи 2,3 модуля 2

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,20 @@
 function isEmpty(obj) {
   // ваш код...
-}
+  for (let key in obj) {
+    if (key in obj) return true;
+  };
+  return false;
+};
+
+let schedule = {};
+alert(isEmpty(schedule));
+
+schedule["8:30"] = "подъём";
+alert(isEmpty(schedule));
+
+delete schedule["8:30"];
+alert(isEmpty(schedule));
+
+schedule['age'] = undefined;
+alert(isEmpty(schedule));
+
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,28 @@
+let num1;
+let num2;
+
 let calculator = {
-  // ваш код
+  // ... ваш код ...
+  read(num1, num2) {
+    this.a = num1;
+    this.b = num2;
+  },
+
+  sum() {
+    return this.a + this.b;
+  },
+
+  mul() {
+    return (this.a * this.b);
+  },
 };
+
+
+
+calculator.read(3, 5);
+console.log(calculator.sum()); // 8
+console.log(calculator.mul()); // 15
+
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
