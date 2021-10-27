4bffa1dd5f4e45ec6f1b627cd732a240e3649d9b
Maxim Demidov <mdemidov@list.ru>
2021-10-27 22:30:33 +0300

    Задача 3 модуля 3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,21 @@
 function camelize(str) {
-  // ваш код...
-}
+
+  let splitStr = str.split('');
+  let newArr = [];
+  for (let i = 0; i < splitStr.length; i++) {
+
+    if (splitStr[i] === '-') {
+      newArr.push(splitStr[i + 1].toUpperCase());
+      splitStr.splice(i, 1);
+    } else newArr.push(splitStr[i]);
+
+  }
+
+  newArr = newArr.join('');
+  return newArr;
+};
+
+console.log(camelize('background-color')) //== 'backgroundColor';
+console.log(camelize('list-style-image')) //== 'listStyleImage';
+console.log(camelize('-webkit-transition')) //== 'WebkitTransition';
+
