874d3e89cf9a17e20f035bf5511a623c830d5fa1
Maxim Demidov <mdemidov@list.ru>
2021-10-29 22:11:28 +0300

    Задача 4 модуль 3

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,14 @@
 function showSalary(users, age) {
-  // ваш код...
+  let filterUsers = users.filter(item => item.age <= age);
+  let userList = '';
+  for (let i = 0; i < filterUsers.length; i++) {
+    userList += `${filterUsers[i].name}, ${filterUsers[i].balance}\n`;
+
+  }
+  userList = userList.trim();
+  return userList;
+
 }
+
+
+showSalary(users, 30);
