dd64a91ad749264f16e9cc48fe973cee168835f2
Maxim Demidov <mdemidov@list.ru>
2021-11-04 18:04:10 +0300

    задача 3 модуль 4

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,54 +1,58 @@
-function indexHeadCell(name) {
+// function indexHeadCell(name) {
+//   table = document.querySelector('table');
+//   let thead = table.querySelector('thead');
+//   let theadRows = thead.querySelectorAll('td');
 
-  table = document.querySelector('table');
-  let theadRows = table.tHead.rows[0].cells;
-
-  let index = 0;
-  for (let i = 0; i < theadRows.length; i++) {
-    if (theadRows[i].textContent == name) {
-      index = i;
-    }
-  }
-  return index;
-};
+//   let index = 0;
+//   for (let i = 0; i < theadRows.length; i++) {
+//     if (theadRows[i].textContent == name) {
+//       index = i;
+//     }
+//   }
+//   return index;
+// };
 
 
 function highlight(table) {
   // ваш код...
+
   let tbody = table.querySelector('tbody');
-  let trBody = tbody.rows;
+  let trBody = tbody.querySelectorAll('tr');
 
-  index = indexHeadCell('Age');
+  // index = indexHeadCell('Age');
   for (let tr of trBody) {
-    if (tr.cells[index].textContent < 18) {
-      tr.cells[index].style.textDecoration = 'line-through';
+    if (tr.cells[1].textContent < 18) {
+      tr.style.textDecoration = 'line-through';
     }
   };
 
-  index = indexHeadCell('Gender');
+
+  // index = indexHeadCell('Gender');
 
   for (let tr of trBody) {
-    if (tr.cells[index].textContent == 'm') {
-      tr.cells[index].classList.add('male');
+    if (tr.cells[2].textContent == 'm') {
+      tr.classList.add('male');
 
     } else {
-      tr.cells[index].classList.add('female');
+      tr.classList.add('female');
     }
   };
 
 
-  index = indexHeadCell('Status');
+  // index = indexHeadCell('Status');
 
   for (let tr of trBody) {
-    if (!tr.cells[index].matches('[data-available]')) {
-      tr.cells[index].hidden = true;
-    } else if (tr.cells[index].dataset.available == 'true') {
-      tr.cells[index].classList.add('available');
+    if (!tr.cells[3].matches('[data-available]')) {
+      tr.hidden = true;
+    } else if (tr.cells[3].dataset.available == 'true') {
+      tr.classList.add('available');
     } else {
-      tr.cells[index].classList.add('unavailable')
+      tr.classList.add('unavailable')
     }
-    console.log(tr.cells[index]);
+    // console.log(tr.cells[index]);
   };
+
+
 
 };
 
